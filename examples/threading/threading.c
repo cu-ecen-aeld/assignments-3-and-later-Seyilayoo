0b679e7d8b96ef6a94ae6121c3bcb0158c014126
Seyilayoo <seyilayoo@gmail.com>
2024-04-30 18:49:58 -0400

    ass 4 p1 fix

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -15,39 +15,25 @@ void* threadfunc(void* thread_param)
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     
     //Initialise the thread parameters by type casting the void pointer
-	struct thread_data* thread_func_args = (struct thread_data *) thread_param;
-	
-	int rc;
-	//Initially set thread complete status as true
-	thread_func_args->thread_complete_success=true;
-	//Suspends the action for the microseconds as specified in the argument
-	//Value in milliseconds converted to microseconds
-	rc=usleep(thread_func_args->wait_ms_obtain*(CONVERT_MS_to_US));
-	if(rc!=0)
-	{
-			thread_func_args->thread_complete_success=false;
-	}
-	//Lock the mutex
-	rc=pthread_mutex_lock(thread_func_args->mutex_lock);
-	if ( rc != 0 )
-	{
-		thread_func_args->thread_complete_success=true;
-	}
-	//Suspends action after locking mutex
-	rc = usleep(thread_func_args->wait_ms_release*(CONVERT_MS_to_US));
-	if(rc!=0)
-	{
-			thread_func_args->thread_complete_success=false;
-	}
-	//Release the mutex lock
-	rc=pthread_mutex_unlock(thread_func_args->mutex_lock);
-	if(rc!=0)
-	{
-			thread_func_args->thread_complete_success=false;
-	}
-    //Equivalent to pthread_exit(3) with the value supplied in the return statement
-   
-	return thread_param;
+	 struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    thread_func_args->thread_complete_success = true;
+
+    usleep(thread_func_args->wait_to_obtain_ms * 1000);
+
+    int rc = pthread_mutex_lock(thread_func_args->mutex);
+    
+    if (rc != 0)
+        thread_func_args->thread_complete_success = false;
+    else
+    {
+        usleep(thread_func_args->wait_to_release_ms * 1000);
+
+        rc = pthread_mutex_unlock(thread_func_args->mutex);
+        if (rc != 0)
+            thread_func_args->thread_complete_success = false;
+    }
+
+    return thread_param;
 }
 
 
